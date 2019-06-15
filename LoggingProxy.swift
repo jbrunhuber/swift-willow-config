20a2717e102d733572a07272f04e9edd7010850c
Joshua Brunhuber <joshua.brunhuber@gmail.com>
2019-06-15 22:33:00 +0200

    Add optional chaining operator
    Add the optional chaining operator to support newer versions of Willow

--- LoggingProxy.swift
+++ LoggingProxy.swift
@@ -8,54 +8,54 @@
 
 import Willow
 
-var logger = LoggingProxy(logger: willow_logger)
+var logger = LoggingProxy(logger: willowLogger)
 
 // swiftformat:disable redundantSelf
 struct LoggingProxy {
 
     var proxLogger: Logger?
-    
+
     init(logger: Logger?) {
         self.proxLogger = logger
     }
-    
-    public func debug(_ message: String, file: String = #file, function: String = #function,  line: Int = #line) {
-        
-        proxLogger.debugMessage(self.format(message: message, file: file, function: function, line: line))
+
+    public func debug(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+
+        proxLogger?.debugMessage(self.format(message: message, file: file, function: function, line: line))
     }
-    
+
     public func info(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
-        
-        proxLogger.infoMessage(self.format(message: message, file: file, function: function, line: line))
+
+        proxLogger?.infoMessage(self.format(message: message, file: file, function: function, line: line))
     }
-    
+
     public func event(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
-        
-        proxLogger.eventMessage(self.format(message: message, file: file, function: function, line: line))
+
+        proxLogger?.eventMessage(self.format(message: message, file: file, function: function, line: line))
     }
 
     public func warn(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
-        
-        proxLogger.warnMessage(self.format(message: message, file: file, function: function, line: line))
+
+        proxLogger?.warnMessage(self.format(message: message, file: file, function: function, line: line))
     }
-    
+
     public func error(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
-        
-        proxLogger.errorMessage(self.format(message: message, file: file, function: function, line: line))
+
+        proxLogger?.errorMessage(self.format(message: message, file: file, function: function, line: line))
     }
-    
+
     private func format(message: String, file: String, function: String, line: Int) -> String {
-        
+
         #if DEBUG /* I use os_log in production where line numbers and functions are discouraged */
             return "[\(sourceFileName(filePath: file)) \(function):\(line)] \(message)"
         #else
             return message
         #endif
     }
-    
+
     private func sourceFileName(filePath: String) -> String {
-        
+
         let components = filePath.components(separatedBy: "/")
-        return components.isEmpty ? "" : components.last!
+        return (components.isEmpty ? "" : components.last) ?? ""
     }
 }
